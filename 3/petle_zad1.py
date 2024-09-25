3d2cca90b83555b5f74dbb95fd411bffcdffd817
rjaniak <rajkonkret660@gmail.com>
2024-09-25 12:05:22 +0200

    [3.4] update petle_zad1.py

--- 3/petle_zad1.py
+++ 3/petle_zad1.py
@@ -26,3 +26,41 @@ for i in range(6):
     lista_wynik.append(wyn)
 
 print(lista_wynik)  # [4, 26, 29, 30, 3, 42]
+
+for i in range(10):
+    if i % 2 == 0:  # modulo, reszta z dzielenia
+        print(i, "parzysta")
+# 0 parzysta
+# 2 parzysta
+# 4 parzysta
+# 6 parzysta
+# 8 parzysta
+
+lista3 = [j for j in range(10) if j % 2 == 0]
+print(lista3)  # [0, 2, 4, 6, 8]
+
+print(lista_wynik)  # [20, 11, 5, 14, 10, 43]
+for c in lista_wynik:
+    if c > 10:
+        print("Większy od 10", c)
+    else:
+        print("Mniejsze od 10", c)
+# Większy od 10 20
+# Większy od 10 11
+# Mniejsze od 10 5
+# Większy od 10 14
+# Mniejsze od 10 10
+# Większy od 10 43
+
+for i in range(0, 10, 2):  # (start, stop, krok)
+    print(i)
+
+for i in range(10, 0, -2):  # ujemny krok, odlicznie w dół
+    print(i)
+# 10
+# 8
+# 6
+# 4
+# 2
+for i in range(-10, 0):
+    print(i)
